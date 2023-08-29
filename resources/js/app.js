0d76565868a6e89616d0ad4825890e49a4592d21
ashok thakur <thakur.ashok9180@gmail.com>
2023-08-29 20:34:17 +0530

    update project

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './components/App.vue';
+import { routes } from './routes';
+
+const app = createApp(App);
+
+app.use(createRouter({
+  history: createWebHistory(),
+  routes: routes
+}));
+
+app.mount('#app');
